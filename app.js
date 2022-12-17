1fae6815b35a2e8a44daf1205b6b9bde2abea56f
Muhammad Abdul Ghani Qureshi <itsaghani@gmail.com>
2022-12-17 21:54:19 +0500

    Form is added

--- app.js
+++ app.js
@@ -6,12 +6,30 @@ const port = 3000;
 
 const host = "127.0.0.1";
 
-app.use("/currenttime", function (req, res) {
-  res.send(`<h1>Hello, Current time is ${new Date().toISOString()}</h1>`);
-});
+app.use(express.urlencoded({ extended: false }));
 
-app.use("/", function (req, res) {
-  res.send(`<h1>Hello World</h1>`);
+app.get("/currenttime", function (req, res) {
+  res.send("<h1>" + new Date().toISOString() + "</h1>");
+});
+// localhost:3000/currenttime
+
+app.get("/", function (req, res) {
+  res.send(
+    `<form action="/store-user" method="POST">
+      <label>Your Name</label>
+      <input type="text" name="username">
+      <button>Submit</button>
+    </form>`
+  );
+});
+// localhost:3000/
+
+app.post("/store-user", function (req, res) {
+  const userName = req.body.username;
+
+  console.log(userName);
+
+  res.send("<h1>Username stored!</h1>");
 });
 
 app.listen(port, console.log(`Running server on https://${host}:${port}`));
